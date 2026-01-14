6f8b23de3a298414f63e179aa0277f295bb23c55
Vitaliy Polyakov <polyakov.vs@mail.ru>
2026-01-14 11:29:29 +0300

    Add main() function

---
+++ ex02_20.c
@@ -0,0 +1,13 @@
+// Converting from seconds to hours, minutes and seconds.
+// Write a program that asks the user to enter the total time elapsed, 
+// in seconds, since an event and converts the time to hours,
+// minutes and seconds. The time should be displayed as hours:minutes:seconds.
+// Hint: Use the remainder operator.
+
+#include <stdio.h>
+
+// function main begins program execution
+int main()
+{
+
+} // end function main

03d17438e069430d281310956feb19cceeaeef2d
Vitaliy Polyakov <polyakov.vs@mail.ru>
2026-01-14 14:32:58 +0300

    Checkerboard Pattern

---
+++ ch02/ex02_27.c
@@ -0,0 +1,22 @@
+// Checkerboard Pattern of Asterisks. Display the following checkerboard pattern
+// with eight printf statements and then display the same pattern with as few 
+// printf statements as possible.
+
+#include <stdio.h>
+
+// function main begins program execution
+int main()
+{
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+
+    printf("\n\n\n");
+
+    printf("* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n");
+} // end function main

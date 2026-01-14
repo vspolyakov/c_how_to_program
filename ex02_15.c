c0eaf01fe71996a611db63d0534328e16801d215
Vitaliy Polyakov <polyakov.vs@mail.ru>
2026-01-14 10:16:15 +0300

    Add first example c-source file

---
+++ ex02_15.c
@@ -0,0 +1,20 @@
+// State the order of evaluation of the operators in each of the following 
+// C statements and show the value of x after each statement is performed.
+//
+// a) x = 8 + 15 * ( 6 – 2) - 1;
+// b) x = 5 % 5 + 5 * 5 - 5 / 5;
+// c) x = ( 5 * 7 * ( 5 + ( 7 * 5 / ( 7 ) ) ) );
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    x = 8 + 15 * (6 - 2) - 1;
+    printf("x = %d\n", x);
+
+    x = 5 % 5 + 5 * 5 - 5 / 5;
+    printf("x = %d\n", x);
+
+    x = (5 * 7 * (5 + (7 * 5 / ( 7 ))));
+    printf("x = %d\n", x);
+}

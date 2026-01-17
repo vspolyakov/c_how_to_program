3bd5866b452fddd0320efa4fd403eea4ce6dffb6
Vitaliy Polyakov <polyakov.vs@mail.ru>
2026-01-17 11:53:20 +0300

    Class-average program with sentinel-controlled iteration

---
+++ ch03/ex03_06.c
@@ -0,0 +1,47 @@
+// Develop a class-averaging program that will process an arbitrary number 
+// of grades each time the program is run.
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+    unsigned int counter;   // number of grades entered
+    int grade;              // grade value
+    int total;              // sum of grades
+    float average;          // number with decimal point for average
+
+    // initialization phase
+    total = 0;      // initialize total
+    counter = 0;    // initialize loop counter
+
+    // processing phase
+    // get first grade from user
+    printf("%s", "Enter grade, -1 to end: ");     // prompt for input
+    scanf("%d", &grade);                          // read grade from user
+
+    // loop while sentinel value not yet read from user
+    while (grade != -1)
+    {
+        total = total + grade; // add grade to total
+        counter = counter + 1; // increment counter
+
+        // get next grade from user
+        printf("%s", "Enter grade, -1 to end: ");   // prompt for input
+        scanf("%d", &grade);                        // read next grade
+    } // end while
+
+    // termination phase
+    // if user entered at least one grade
+    if (counter != 0)
+    {
+        // calculate average of all grades entered
+        average = (float) total / counter; // avoid truncation
+        // display average with two digits of precision
+        printf("Class average is %.2f\n", average);
+    } // end if
+    else
+    {
+        // if no grades were entered, output message
+        puts("No grades were entered.\n");
+    } // end else
+} // end function main()
